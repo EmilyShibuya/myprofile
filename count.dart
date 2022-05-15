c6c6d172de89649eb57d7a0754f0da8367f21516
EmilyShibuya <105578438+EmilyShibuya@users.noreply.github.com>
2022-05-15 15:11:02 +0900

    Add files via upload

---
+++ count.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+part 'count.g.dart';
+@HiveType(typeId: 1)
+class Count {
+  @HiveField(0)
+  int count;
+  Count({required this.count});
+}
